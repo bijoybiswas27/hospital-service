0793cbc297f781a200c719c66b222c9434ebba10
Bijoy Kumar Biswas <bijoybiswas27@gmail.com>
2024-12-01 20:36:33 +0530

    initial commit

---
+++ src/main/java/com/bijoy/events/hospital_service/config/RabbitMQConfig.java
@@ -0,0 +1,65 @@
+package com.bijoy.events.hospital_service.config;
+
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${patient.exchange.name}")
+    private String patientExchangeName;
+    @Value("${patient.creation.queue.name}")
+    private String patientCreationQueueName;
+    @Value("${patient.charge.queue.name}")
+    private String patientChargeQueueName;
+    @Value("${patient.creation.binding}")
+    private String patientCreationRoutingKey;
+    @Value("${patient.charge.binding}")
+    private String patientChargeRoutingKey;
+
+    @Bean
+    public Queue patientCreationQueue() {
+        return new Queue(patientCreationQueueName);
+    }
+
+    @Bean
+    public Queue patientChargeQueue() {
+        return new Queue(patientChargeQueueName);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(patientExchangeName);
+    }
+
+    @Bean
+    public Binding patientCreationBinding() {
+        return BindingBuilder.bind(patientCreationQueue())
+                .to(exchange())
+                .with(patientCreationRoutingKey);
+    }
+
+    @Bean
+    public Binding patientChargeBinding() {
+        return BindingBuilder.bind(patientCreationQueue())
+                .to(exchange())
+                .with(patientChargeRoutingKey);
+    }
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
